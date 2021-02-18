a731f11516757d8ac90001f0f682ae2ef52c7ce1
hamzabinsarfraz <muhammadameerhmza@gmail.com>
2021-02-18 15:39:44 +0500

    change staging server url

--- src/axios-instance.js
+++ src/axios-instance.js
@@ -1,8 +1,8 @@
 const axios = require('axios');
 
 const apiInstance = axios.create({
-    // baseURL: 'https://sabhi-sdr-test-node.herokuapp.com/',
-    baseURL: 'http://localhost:2011/',
+    baseURL: 'https://sabhi-sdr-test-node.herokuapp.com/',
+    // baseURL: 'http://localhost:2011/',
 });
 
 module.exports = {

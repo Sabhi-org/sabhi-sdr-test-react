2c87baf0d07bd9c552765de25c63a8d94221bfb8
aitizaz-ul-haq <atz.softprgmr@gmail.com>
2021-02-26 16:22:07 +0500

    ui changes to sdr form

--- src/components/Antd-form.js
+++ src/components/Antd-form.js
@@ -1,9 +1,7 @@
 // importing antd components------------------------->
 import React from 'react';
-import { Form, Input, Button, Col, Row, Divider } from 'antd';
-
-
-
+import { Form, Input, Button, Col, Row, Divider, Image, Avatar } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
 
 
 // creating antdform----------------->
@@ -16,6 +14,9 @@ export default function Antdform() {
         console.log('Failed:', errorInfo);
     };
 
+
+
+
     // rendering antd form ---------------------->
     return (
         <div>
@@ -27,73 +28,136 @@ export default function Antdform() {
                 onFinish={onFinish}
                 onFinishFailed={onFinishFailed}>
                 <Row>
-                    <Col span={12}>
-                        <Form.Item
+                    <Col span={4}>
+                        <Avatar
+                            style={{ marginTop: "18px" }}
+                            src={<Image src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png" />}
+                            size={140}
+                        />
+                    </Col>
+                    <Col span={20}>
+                        <Row gutter={16}>
+                            <Col className="gutter-row" span={2}>
+                            </Col>
+                            <Col className="gutter-row" span={11}>
+                                <Form.Item
+                                    label="Name"
+                                    name="fullName"
+                                    rules={[{ required: true, message: 'Please enter Complete Name' }]}>
+                                    <Input size="small" />
+                                </Form.Item>
+                            </Col>
+                            <Col className="gutter-row" span={11}>
+                                <Form.Item
+                                    label="Father Name"
+                                    name="fatherName"
+                                    rules={[{ required: true, message: 'Please enter Father Name' }]}>
+                                    <Input size="small" />
+                                </Form.Item>
+                            </Col>
+                        </Row>
+                        <Row gutter={16} offset={4}>
+                            <Col className="gutter-row" span={2}>
+                                {/* <div style={style}>col-6</div> */}
+                            </Col>
+                            <Col className="gutter-row" span={11}>
+                                <Form.Item
+                                    label="Gender"
+                                    name="gender"
+                                    rules={[{ required: true, message: 'Please enter gender' }]}>
+                                    <Input size="small" />
+                                </Form.Item>
+                            </Col>
+                            <Col className="gutter-row" span={11}>
+                                <Form.Item
+                                    label="Country of Stay"
+                                    name="countryOfStay"
+                                    rules={[{ required: true, message: 'Please enter the country of stay' }]}>
+                                    <Input size="small" />
+                                </Form.Item>
+                            </Col>
+                            <Col className="gutter-row" span={6}>
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+                <Row>
+                    <Row gutter={22}>
+                        <Col className="gutter-row" span={6}>
+                            <Form.Item
+                                label="Identity Number"
+                                name="identityNumber"
+                                rules={[{ required: true, message: 'Enter the Identity Number' }]}>
+                                <Input size="small" />
+                            </Form.Item>
+                        </Col>
 
-                            label="Name"
-                            name="fullName"
-                            rules={[{ required: true, message: 'Please enter Complete Name' }]}>
-                            <Input />
-                        </Form.Item>
-                        <Form.Item
-
-                            label="Gender"
-                            name="gender"
-                            rules={[{ required: true, message: 'Please enter gender' }]}>
-                            <Input />
-                        </Form.Item>
+                        <Col className="gutter-row" span={6}>
+                            <Form.Item
+                                label="Date of Issue"
+                                name="issueDate"
+                                rules={[{ required: true, message: 'Enter Date of Issue' }]}>
+                                <Input size="small" />
+                            </Form.Item>
+                        </Col>
+                        <Col className="gutter-row" span={6}>
+                            <Form.Item
+                                label="Date of Birth"
+                                name="birthDate"
+                                rules={[{ required: true, message: 'Enter Date of Birth' }]}>
+                                <Input size="small" />
+                            </Form.Item>
+                        </Col>
+                        <Col className="gutter-row" span={6}>
+                            <Form.Item
+                                label="Date of Expiry"
+                                name="expireDate"
+                                rules={[{ required: true, message: 'Enter expiry Date' }]}>
+                                <Input size="small" />
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                    {/* <Col span={12}>
                         <Form.Item
                             label="Identity Number"
                             name="identityNumber"
                             rules={[{ required: true, message: 'Please enter the Identity Number' }]}>
-                            <Input />
+                            <Input size="small" />
                         </Form.Item>
                         <Form.Item
                             label="Date of Issue"
                             name="issueDate"
                             rules={[{ required: true, message: 'Please enter date of issuance' }]}>
-                            <Input />
+                            <Input size="small" />
                         </Form.Item>
-                    </Col>
-                    <Divider type="vertical" />
-                    <Col span={11}>
-                        <Form.Item
-                            label="Father Name"
-                            name="fatherName"
-                            rules={[{ required: true, message: 'Please enter Father Name' }]}>
-                            <Input />
-                        </Form.Item>
-                        <Form.Item
-                            label="Country of Stay"
-                            name="countryOfStay"
-                            rules={[{ required: true, message: 'Please enter the country of stay' }]}>
-                            <Input />
-                        </Form.Item>
+                    </Col> */}
+                    {/* <Divider type="vertical" /> */}
+                    {/* <Col span={11}>
                         <Form.Item
                             label="Date of Birth"
                             name="birthDate"
                             rules={[{ required: true, message: 'Please enter Date of Birth' }]}>
-                            <Input />
+                            <Input size="small" />
                         </Form.Item>
                         <Form.Item
                             label="Date of Expiry"
                             name="expireDate"
                             rules={[{ required: true, message: 'Please enter Date of expiry' }]}>
-                            <Input />
+                            <Input size="small" />
                         </Form.Item>
-                    </Col>
+                    </Col> */}
                     <Col span={24}>
                         <Form.Item
                             label="Permanent Address"
                             name="permanentAddress"
                             rules={[{ required: true, message: 'Please enter permanent address' }]}>
-                            <Input />
+                            <Input size="small" />
                         </Form.Item>
                         <Form.Item
                             label="Temporary Address"
                             name="temporaryAddress"
                             rules={[{ required: true, message: 'Please enter temporary address' }]}>
-                            <Input />
+                            <Input size="small" />
                         </Form.Item>
                     </Col>
                 </Row>
@@ -107,7 +171,7 @@ export default function Antdform() {
                     </Col>
                 </Row>
             </Form>
-        </div>
+        </div >
     );
 
 } 

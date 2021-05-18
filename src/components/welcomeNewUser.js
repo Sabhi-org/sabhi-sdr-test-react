74b35067478a49377741832692ac55087240c36a
aitizaz-ul-haq <atz.softprgmr@gmail.com>
2021-05-18 13:04:00 +0500

    completed welcome user screen

--- src/components/welcomeNewUser.js
+++ src/components/welcomeNewUser.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Col, Row, Typography } from 'antd';
+import { ShrinkOutlined, QrcodeOutlined, CodepenOutlined, SearchOutlined, RightOutlined } from '@ant-design/icons';
 
 import '../welcome.css';
 
@@ -10,42 +11,122 @@ export default function welcomeNewUser() {
     const { Text } = Typography;
 
     return (
-        <div className="background">
+        <div className="cover">
 
+            <div className="background">
 
-            <div className="fontone">Welcome,</div>
-            <div className="fonttwo">New User!</div><br></br>
-            <div className="fontthree">Profile</div><br></br>
+                {/* top font section of screen */}
 
-
-            <Row>
-                <Col span={8}>
-                    <div className="buttonone" type="primary" shape="round" size='large'>
-                        <p className="buttononefont">Personal</p>
-                    </div>
-                </Col>
-                <Col span={16}>
-                    <div className="buttontwo" type="primary" shape="round" size='large'>
-                        <p className="buttontwofont">+ Add New Buisness</p>
-                    </div>
-                </Col>
-            </Row>
-
-
-            <div className="fontfour">Personal Information</div>
-
-            <Col span={24}>
-                <Row>
-                    <div className="buttonthree" type="primary" shape="round" size='large'>
-                        <p className="buttontwofont">Scan ID Card</p>
-                    </div>
+                <Row span={24}>
+                    <Col span={22}>
+                        <div className="fontone">Welcome,</div>
+                        <div className="fonttwo">New User!</div>
+                    </Col>
+                    <Col>
+                        <div className="block2"></div>
+                    </Col>
                 </Row>
+
+
                 <Row>
-                    <div className="buttonfour" type="primary" shape="round" size='large'>
-                        <p className="buttontwofont">Record Selfie</p>
-                    </div>
+                    <Col span={22}>
+                        <div className="fontthree">Profile</div>
+                    </Col>
+                    <Col>
+                        <SearchOutlined style={{ fontSize: '20px', color: '#35568D', marginLeft: '6px', marginTop: '30px' }} />
+                    </Col>
                 </Row>
-            </Col>
+
+                <br></br>
+
+
+
+                {/* first set of buttons side by side */}
+
+                <Row span={20}>
+                    <Col span={8}>
+                        <div className="buttonone" type="primary" shape="round" size='large'>
+                            <p className="buttononefont">Personal</p>
+                        </div>
+                    </Col>
+                    <Col span={12}>
+                        <div className="buttontwo" type="primary" shape="round" size='large'>
+                            <p className="buttontwofont">+ Add New Buisness</p>
+                        </div>
+                    </Col>
+                </Row>
+
+                {/* personel information section */}
+
+                <div className="fontfour">Personal Information</div>
+
+                {/* scan id section */}
+
+                <Col span={24}>
+                    <Row>
+                        <div className="buttonthree" type="primary" shape="round" size='large'>
+                            <Col span={24}>
+                                <Row>
+                                    <Col span={4}>
+                                        <div className="block"></div>
+                                    </Col>
+                                    <Col span={16}>
+                                        <p className="buttonthreefont">Scan ID Card</p>
+                                        <p className="pending">Pending</p>
+                                    </Col>
+                                    <Col>
+                                        <RightOutlined style={{ fontSize: '14px', color: '#95A7C6', marginLeft: '0px', marginTop: '30px' }} />
+                                    </Col>
+                                </Row>
+                            </Col>
+                        </div>
+                    </Row>
+
+                    {/* record sefie section */}
+
+                    <Row>
+                        <div className="buttonfour" type="primary" shape="round" size='large'>
+                            <Col span={24}>
+                                <Row>
+                                    <Col span={4}>
+                                        <div className="block"></div>
+                                    </Col>
+                                    <Col span={16}>
+                                        <p className="buttonfourfont">Record Selfie</p>
+                                        <p className="pending">Pending</p>
+                                    </Col>
+                                    <Col>
+                                        <RightOutlined style={{ fontSize: '14px', color: '#95A7C6', marginLeft: '0px', marginTop: '30px' }} />
+                                    </Col>
+                                </Row>
+                            </Col>
+                        </div>
+                    </Row>
+                </Col>
+
+                {/* end of personel information section */}
+
+            </div>
+
+            {/* bottom section of the screen */}
+
+            <div className="bottombar">
+                <div className="QRcodecircle"><QrcodeOutlined style={{ fontSize: '37px', color: 'white', marginLeft: '13px', marginTop: '12px' }} /></div>
+                <Row className="bar">
+                    <Col span={8}>
+                        <ShrinkOutlined style={{ fontSize: '30px', color: '#35568D', marginLeft: '49px', marginTop: '17px' }} />
+                        <p className="left">Profile</p>
+                    </Col>
+                    <Col span={8}>
+                        <p className="center">Scan QR Code</p>
+                    </Col>
+                    <Col span={8}>
+                        <CodepenOutlined style={{ fontSize: '33px', color: '#95A7C6', marginLeft: '49px', marginTop: '12px', marginBottom: '4px' }} />
+                        <p className="right">Applications</p>
+                    </Col>
+
+                </Row>
+            </div>
         </div>
     );
 }

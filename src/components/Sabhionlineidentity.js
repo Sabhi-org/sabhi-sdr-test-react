292835713c4001078cb83ccd966462d760288988
aitizaz-ul-haq <atz.softprgmr@gmail.com>
2021-05-20 17:42:58 +0500

    added more ui elements to terms and conditions screen

--- src/components/Sabhionlineidentity.js
+++ src/components/Sabhionlineidentity.js
@@ -9,7 +9,7 @@ export default function Sabhionlineidentity() {
 
 
     return (
-        <div className="cover">
+        <div className="coverin">
 
 
             <Row>
--- src/components/TermsandCOnditions.js
+++ src/components/TermsandCOnditions.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Col, Row } from 'antd';
-import { LeftOutlined, QuestionCircleOutlined, MoreOutlined } from '@ant-design/icons';
+import { LeftOutlined, QuestionCircleOutlined, MoreOutlined, RightOutlined, BorderOutlined } from '@ant-design/icons';
 import '../styles/termsandc.css';
 
 export default function TermsandCOnditions() {
@@ -34,7 +34,7 @@ export default function TermsandCOnditions() {
                             <p className="termstext">Terms and</p>
                         </div>
                         <div className="termstitletwo">
-                            <p>Conditions</p>
+                            <p className="condi">Conditions</p>
                         </div>
                     </Col>
                 </Row>
@@ -57,8 +57,60 @@ export default function TermsandCOnditions() {
                         <p className="termstextpara">you'll be agreeing to the following</p>
                         <p className="termstextpara">terms and condition</p>
                     </div>
-
                 </Row>
+
+
+                <Col span={24}>
+                    <Row>
+                        <div className="buttonthreeinterms" type="primary" shape="round" size='large'>
+                            <Col span={24}>
+                                <Row>
+                                    <Col span={4}>
+                                        <div className="blockoneinterms"></div>
+                                    </Col>
+                                    <Col span={16}>
+                                        <p className="buttonthreeintermsfont">Over 18 years</p>
+                                        <p className="termsageverification">Age Verification</p>
+                                    </Col>
+                                    <Col>
+                                        <BorderOutlined style={{ fontSize: '18px', color: '#95A7C6', marginLeft: '0px', marginTop: '30px' }} />
+                                    </Col>
+                                </Row>
+                            </Col>
+                        </div>
+                    </Row>
+
+
+
+                    <Row>
+                        <div className="buttonfourinterms" type="primary" shape="round" size='large'>
+                            <Col span={24}>
+                                <Row>
+                                    <Col span={4}>
+                                        <div className="blocktwointerms"></div>
+                                    </Col>
+                                    <Col span={16}>
+                                        <p className="buttonfourintermsfont">Pakistan</p>
+                                        <p className="termsageverificationtwo">Country Verification</p>
+                                    </Col>
+                                    <Col>
+                                        <BorderOutlined style={{ fontSize: '18px', color: '#95A7C6', marginLeft: '0px', marginTop: '30px' }} />
+                                    </Col>
+                                </Row>
+                            </Col>
+                        </div>
+                    </Row>
+                </Col>
+
+                <Row>
+                    <div className="containstermstext">
+                        <p className="termstextpara"><small><b>terms and Conditions</b></small></p>
+                        <p className="termstextpara">Lorem ipsum dolor sit amet,</p>
+                        <p className="termstextpara">consectetur adipiscing elit,</p>
+                        <p className="termstextpara">sed do eiusmod tempor incididunt</p>
+                    </div>
+                </Row>
+
             </div>
         </div>
     );

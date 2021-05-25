9cd97f2ca2f6455670e7b072c70ecd9e7d016744
hamzabinsarfraz <muhammadameerhmza@gmail.com>
2021-05-25 13:06:14 +0500

    some changes

--- src/App.js
+++ src/App.js
@@ -45,6 +45,10 @@ export default function App() {
         <Route path='/phonev' component={Phonev} />
         <Route path='/smsverify' component={Smsverify} />
         <Route path='/idready' component={Idready} />
+        <Route path='/ocr_front' component={IdentityCardFrontScan} />
+        <Route path='/ocr_back' component={IdentityCardBackScan} />
+        <Route path='/form_sdr' component={antsdrform} />
+        <Route path='/user_profile' component={UserProfile} /> 
         {/* <Route path='/' component={SplashScreen} exact />
         <Route path='/onboard' component={OnBoardScreen} />
         <Route path='/ocr_front' component={IdentityCardFrontScan} />

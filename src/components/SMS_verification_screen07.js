58ca59f4a625aba84ac8ce953c522c669068938b
aitizaz <36325075+aitizaz-ul-haq@users.noreply.github.com>
2021-06-01 18:02:16 +0500

    Update SMS_verification_screen07.js

--- src/components/SMS_verification_screen07.js
+++ src/components/SMS_verification_screen07.js
@@ -1,24 +1,73 @@
-import React from 'react'; import { Col, Row } from 'antd';
+import React from 'react'; 
+import { Col, message, Row, Statistic, Spin } from 'antd';
 import { useHistory } from 'react-router-dom';
 import { LeftOutlined, QuestionCircleOutlined, MoreOutlined } from '@ant-design/icons';
 import '../styles/SMS_verification_screen07.css';
+import { sabhiApiInstance } from '../axios-instance';
 
 export default function Smsverify() {
     let history = useHistory();
-    function tothenext() {
+    const { Countdown } = Statistic;
+    const [first, setFirst] = useState(0);
+    const [second, setSecond] = useState(0);
+    const [third, setThird] = useState(0);
+    const [four, setFour] = useState(0);
+    const [isLoading, setLoading] = useState(false);
 
-
-        history.push('/idready');
-        console.log('clicked');
-
+    async function confirmVerificationCode() {
+        try {
+            const verifyOtpValue = first + second + third + four;
+            const response = await sabhiApiInstance.post('/otp/verify', {
+                otp: verifyOtpValue,
+            });
+            if (!response.data.status) message.info(response.data.message);
+            if (response.data.status) {
+                message.success('otp verified!');
+                history.push('/idready');
+            }
+        } catch (error) {
+            message.error('something went wrong please try again!');
+        }
     }
 
-    const numOfFields = 4;
+    function onChange(val) {
+        if (4.95 * 1000 < val && val < 5 * 1000) {
+            console.log('changed!');
+        }
+    }
+
+    async function resendCode() {
+        try {
+            setLoading(true);
+            const phoneNumber = localStorage.getItem('PhoneNumber');
+            const response = await sabhiApiInstance.post('otp', {
+                phoneNumber: phoneNumber,
+            });
+            if (response.data.status) {
+                message.success('otp resent successfully!');
+                setLoading(false);
+            } else {
+                message.info('something went wrong please try again');
+            }
+        } catch (error) {
+            message.error('something went wrong please try again!');
+        }
+    }
 
     const handleChange = e => {
         const { maxLength, value, name } = e.target;
-        const [fieldName, fieldIndex] = name.split("-");
+        switch (name) {
+            case 'ssn-1':
+                setFirst(value);
+            case 'ssn-2':
+                setSecond(value);
+            case 'ssn-3':
+                setThird(value);
+            case 'ssn-4':
+                setFour(value);
+        }
 
+        const [fieldName, fieldIndex] = name.split("-");
         // Check if they hit the max character length
         if (value.length >= maxLength) {
             // Check if it's not the last input field
@@ -27,15 +76,16 @@ export default function Smsverify() {
                 const nextSibling = document.querySelector(
                     `input[name=ssn-${parseInt(fieldIndex, 10) + 1}]`
                 );
-
                 // If found, focus the next field
                 if (nextSibling !== null) {
                     nextSibling.focus();
                 }
             }
         }
+
     }
     return (
+        <Spin spinning={isLoading}>
         <div className="smsvscreen">
             <div className="containssms">
                 <Row>
@@ -94,7 +144,7 @@ export default function Smsverify() {
                 <Row>
                     <Col span={24}>
                         <Col span={9} offset={8}>
-                            <p className="resendsms">Resend Code</p>
+                            <p className="resendsms" onClick={resendCode}>Resend Code</p>
                         </Col>
                         <Col span={9} offset={7}>
                             <p className="resendsmstwo">Wait 05:00 mins</p>
@@ -104,7 +154,7 @@ export default function Smsverify() {
                 <Row span={24}>
                     <div className="downed">
                         <Col span={21}>
-                            <div className="smsvendbutton" onClick={tothenext} type="primary" shape="round" size='large'>
+                            <div className="smsvendbutton" onClick={confirmVerificationCode} type="primary" shape="round" size='large'>
                                 <p className="confirmsmsv">Confirm</p>
                             </div>
                         </Col>
@@ -112,5 +162,6 @@ export default function Smsverify() {
                 </Row>
             </div>
         </div>
+ </Spin>
     );
 }

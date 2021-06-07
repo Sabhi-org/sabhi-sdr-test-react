e7538540864d3767f9e064e8b14bbd6aed53067c
aitizaz-ul-haq <atz.softprgmr@gmail.com>
2021-06-07 19:34:21 +0500

    camera integartion in progress

--- src/index.js
+++ src/index.js
@@ -7,11 +7,12 @@ import 'antd/dist/antd.css';
 import Swal from 'sweetalert2/dist/sweetalert2.js'
 import 'sweetalert2/src/sweetalert2.scss'
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
   document.getElementById('root')
-);
+); serviceWorker.register();
 

82f858808c0e147d9a8abeeecf39ae6395203380
hamzabinsarfraz <muhammadameerhmza@gmail.com>
2021-06-08 23:04:08 +0500

    add some changes to cnic form

--- src/components/National_IdentityCard_CNIC_screen16.js
+++ src/components/National_IdentityCard_CNIC_screen16.js
@@ -1,16 +1,83 @@
 
 // importing react and its components----->
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
-
+import { sabhiApiInstance, apiInstance } from '../axios-instance';
 // importing antd components------------->
-import { Col, Row } from 'antd';
+import { Form, Input, Button, Col, Row, Image, Avatar, Spin, Space } from 'antd';
 import { LeftOutlined, QuestionCircleOutlined, MoreOutlined } from '@ant-design/icons';
 
 // importing css style files----------------------------------->
 import '../styles/National_IdentityCard_CNIC_screen16.css';
 
 export default function Nationalidentitycard() {
+
+    const did = localStorage.getItem('DID');
+    let history = useHistory();
+    const [form] = Form.useForm();
+    const [isLoading, setIsloading] = useState('false');
+    const [images, setImages] = useState({});
+
+    const getOcrRecord = async () => {
+        try {
+            setIsloading(true);
+            const response = await sabhiApiInstance.get(`ocr/${did}`);
+            const { identityCardFrontData, identityCardBackData, profileImage, identityCardFrontImage, identityCardBackImage } = response.data.data[0];
+            setImages({ profileImage: profileImage, identityCardFrontImage: identityCardFrontImage, identityCardBackImage: identityCardBackImage });
+            form.setFieldsValue({
+                fullName: identityCardFrontData.nameEnglish,
+                fatherName: identityCardFrontData.fatherNameEnglish,
+                gender: identityCardFrontData.gender,
+                countryOfStay: identityCardFrontData.countryOfStay,
+                identityNumber: identityCardFrontData.identityNumber,
+                issueDate: identityCardFrontData.dateOfIssue,
+                birthDate: identityCardFrontData.dateOfBirth,
+                expireDate: identityCardFrontData.dateOfExpiry,
+                permanentAddress: identityCardBackData.permenantAddress,
+                temporaryAddress: identityCardBackData.presentAddress,
+            });
+            setIsloading(false);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        getOcrRecord();
+    }, []);
+
+
+    const createVerifiablePresentation = async (values) => {
+        try {
+            const response = await apiInstance.post('vp', values);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    const onFinish = (values) => {
+        console.log(values);
+        setIsloading(true);
+        values.profileImage = images.profileImage;
+        values.did = did;
+        values.identityCardBackImage = images.identityCardBackImage;
+        values.identityCardFrontImage = images.identityCardFrontImage;
+        createVerifiablePresentation(values);
+        setIsloading(false);
+        history.push({
+            pathname: '/user_profile',
+        });
+    };
+
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
+
+    function goUserScreen() {
+        history.push('/welcome');
+    }
+
+
     return (
         <div className="cnicscreen">
             <div className="containsnic">
@@ -53,118 +120,131 @@ export default function Nationalidentitycard() {
                         <p className="cnicnationaltextpara">preview the scanned document.</p>
                     </div>
                 </Row>
-                <Row>
-                    <Col span={18}>
-                        <small className="tinytextinnicscreen">Full Name</small>
-                    </Col>
-                </Row>
-                <Row span={24}>
-                    <Col span={24}>
-                        <input className="inputsinpersocnicscreens"></input>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col span={18}>
-                        <small className="tinytextinpersonalscreen">Last Name</small>
-                    </Col>
-                </Row>
-                <Row span={24}>
-                    <Col span={24}>
-                        <input className="inputsinpersonalscreen"></input>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col span={18}>
-                        <small className="tinytextinpersonalscreen">Street Address</small>
-                    </Col>
-                </Row>
-                <Row span={24}>
-                    <Col span={24}>
-                        <input className="inputsinpersonalscreen"></input>
-                    </Col>
-                </Row>
-                <Row span={24}>
-                    <Col span={12}>
-                        <Row>
-                            <Col span={18}>
-                                <small className="tinytextinpersonalscreen">City</small>
-                            </Col>
-                        </Row>
-                        <Row span={24}>
-                            <Col span={24}>
-                                <input className="inputsincnicscreengender"></input>
-                            </Col>
-                        </Row>
-                    </Col>
-                    <Col span={12}>
-                        <Row>
-                            <Col span={18}>
-                                <small className="tinytextinpersonalscreen">Postal Code</small>
-                            </Col>
-                        </Row>
-                        <Row span={24}>
-                            <Col span={24}>
-                                <input className="inputsincnicscreenstay"></input>
-                            </Col>
-                        </Row>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col span={18}>
-                        <small className="tinytextinpersonalscreen">Country of Residence</small>
-                    </Col>
-                </Row>
-                <Row span={24}>
-                    <Col span={24}>
-                        <input className="inputsinpersonalscreen"></input>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col span={18}>
-                        <small className="tinytextinpersonalscreen">CNIC number</small>
-                    </Col>
-                </Row>
-                <Row span={24}>
-                    <Col span={24}>
-                        <input className="inputsinpersonalscreen"></input>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col span={18}>
-                        <small className="tinytextinpersonalscreen">Date of Birth</small>
-                    </Col>
-                </Row>
-                <Row span={24}>
-                    <Col span={24}>
-                        <input className="inputsinpersonaldate"></input>
-                    </Col>
-                </Row>
-                <Row span={24}>
-                    <Col span={12}>
-                        <Row>
-                            <Col span={18}>
-                                <small className="tinytextinpersonalscreen">Issue Date</small>
-                            </Col>
-                        </Row>
-                        <Row span={24}>
-                            <Col span={24}>
-                                <input className="inputsinpersonalissue"></input>
-                            </Col>
-                        </Row>
-                    </Col>
-                    <Col span={12}>
-                        <Row>
-                            <Col span={18}>
-                                <small className="tinytextinpersonalscreen">Expiry Date</small>
-                            </Col>
-                        </Row>
-                        <Row span={24}>
-                            <Col span={24}>
-                                <input className="inputsinpersonalscreen"></input>
-                            </Col>
-                        </Row>
-                    </Col>
-                </Row>
+
+
+
+                <Form
+                    layout="horizontal"
+                    form={form}
+                    name="basic"
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}>
+
+
+                    <Row>
+                        <Col span={18}>
+                            <small className="tinytextinnicscreen">Full Name</small>
+                        </Col>
+                    </Row>
+                    <Row span={24}>
+                        <Col span={24}>
+                            <input className="inputsinpersocnicscreens"></input>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col span={18}>
+                            <small className="tinytextinpersonalscreen">Last Name</small>
+                        </Col>
+                    </Row>
+                    <Row span={24}>
+                        <Col span={24}>
+                            <input className="inputsinpersonalscreen"></input>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col span={18}>
+                            <small className="tinytextinpersonalscreen">Street Address</small>
+                        </Col>
+                    </Row>
+                    <Row span={24}>
+                        <Col span={24}>
+                            <input className="inputsinpersonalscreen"></input>
+                        </Col>
+                    </Row>
+                    <Row span={24}>
+                        <Col span={12}>
+                            <Row>
+                                <Col span={18}>
+                                    <small className="tinytextinpersonalscreen">City</small>
+                                </Col>
+                            </Row>
+                            <Row span={24}>
+                                <Col span={24}>
+                                    <input className="inputsincnicscreengender"></input>
+                                </Col>
+                            </Row>
+                        </Col>
+                        <Col span={12}>
+                            <Row>
+                                <Col span={18}>
+                                    <small className="tinytextinpersonalscreen">Postal Code</small>
+                                </Col>
+                            </Row>
+                            <Row span={24}>
+                                <Col span={24}>
+                                    <input className="inputsincnicscreenstay"></input>
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col span={18}>
+                            <small className="tinytextinpersonalscreen">Country of Residence</small>
+                        </Col>
+                    </Row>
+                    <Row span={24}>
+                        <Col span={24}>
+                            <input className="inputsinpersonalscreen"></input>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col span={18}>
+                            <small className="tinytextinpersonalscreen">CNIC number</small>
+                        </Col>
+                    </Row>
+                    <Row span={24}>
+                        <Col span={24}>
+                            <input className="inputsinpersonalscreen"></input>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col span={18}>
+                            <small className="tinytextinpersonalscreen">Date of Birth</small>
+                        </Col>
+                    </Row>
+                    <Row span={24}>
+                        <Col span={24}>
+                            <input className="inputsinpersonaldate"></input>
+                        </Col>
+                    </Row>
+                    <Row span={24}>
+                        <Col span={12}>
+                            <Row>
+                                <Col span={18}>
+                                    <small className="tinytextinpersonalscreen">Issue Date</small>
+                                </Col>
+                            </Row>
+                            <Row span={24}>
+                                <Col span={24}>
+                                    <input className="inputsinpersonalissue"></input>
+                                </Col>
+                            </Row>
+                        </Col>
+                        <Col span={12}>
+                            <Row>
+                                <Col span={18}>
+                                    <small className="tinytextinpersonalscreen">Expiry Date</small>
+                                </Col>
+                            </Row>
+                            <Row span={24}>
+                                <Col span={24}>
+                                    <input className="inputsinpersonalscreen"></input>
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
+
+                </Form>
                 <br></br>
                 <br></br>
                 <br></br>
@@ -180,7 +260,7 @@ export default function Nationalidentitycard() {
                                 </div>
                             </Col>
                             <Col span={9} offset={1}>
-                                <div className="buttonincnicscreen2" shape="round" size='large'>
+                                <div className="buttonincnicscreen2" onClick={goUserScreen} shape="round" size='large'>
                                     <p className="continuecnicscreen2">Done</p>
                                 </div>
                             </Col>
